290016f06292c5a157b57cf86baba1e7e1976dfc
Naim Sedán <naim.alejandro.sedan@gmail.com>
2020-06-18 15:16:10 +0100

    Onclick remove from HTML, now in .js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,6 +11,21 @@ $(".fa-power-off").click(function () {
     $('#cyan').removeClass('cyan-off').addClass('cyan-on');
 });
 
+/* VARIABLES */
+
+let level = 1;
+let i;
+let j;
+let random;
+let interval;
+
+
+
+
+
+
+
+
 
 /* SOUND EFFECTS */
 let audio1 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
@@ -18,40 +33,88 @@ let audio2 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
 let audio3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
 let audio4 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
-function playRedAudio() {
+
+$('#red').click(function () {
     if ($('#red').hasClass('red-on')) {
+        $('#red').addClass('red-active');
         audio1.play();
-        audio4.play();
-    }
-}
 
-function playGreenAudio() {
+        setTimeout(function () {
+            $('#red').removeClass('red-active');
+        }, 250);
+    }
+});
+
+$('#green').click(function () {
     if ($('#green').hasClass('green-on')) {
+        $('#green').addClass('green-active');
         audio2.play();
-        audio3.play();
-    }
-}
 
-function playYellowAudio() {
+        setTimeout(function () {
+            $('#green').removeClass('green-active');
+        }, 250);
+    }
+});
+
+$('#yellow').click(function () {
     if ($('#yellow').hasClass('yellow-on')) {
+        $('#yellow').addClass('yellow-active');
         audio1.play();
-    }
-}
-
-function playBlueAudio() {
-    if ($('#blue').hasClass('blue-on')) {
-        audio2.play();
-    }
-}
-
-function playPurpleAudio() {
-    if ($('#purple').hasClass('purple-on')) {
-        audio3.play();
-    }
-}
-
-function playCyanAudio() {
-    if ($('#cyan').hasClass('cyan-on')) {
         audio4.play();
+
+        setTimeout(function () {
+            $('#yellow').removeClass('yellow-active');
+        }, 250);
     }
-}
+});
+
+$('#blue').click(function () {
+    if ($('#blue').hasClass('blue-on')) {
+        $('#blue').addClass('blue-active');
+        audio2.play();
+        audio3.play();
+
+        setTimeout(function () {
+            $('#blue').removeClass('blue-active');
+        }, 250);
+    }
+});
+
+$('#purple').click(function () {
+    if ($('#purple').hasClass('purple-on')) {
+        $('#purple').addClass('purple-active');
+        audio3.play();
+
+        setTimeout(function () {
+            $('#purple').removeClass('purple-active');
+        }, 250);
+    }
+});
+
+$('#cyan').click(function () {
+    if ($('#cyan').hasClass('cyan-on')) {
+        $('#cyan').addClass('cyan-active');
+        audio4.play();
+
+        setTimeout(function () {
+            $('#cyan').removeClass('cyan-active');
+        }, 250);
+    }
+});
+
+
+/* PLAY BUTTON*/
+$(".fa-play").click(function () {
+    if ($('#cyrene').hasClass('logo-on')) {
+        $('#level-message').text('1')
+        /*starts game*/
+    }
+});
+
+/* RESTART BUTTON */
+$(".fa-undo").click(function () {
+    if ($('#cyrene').hasClass('logo-on')) {
+        $('#level-message').text('Press Start')
+        /*stops game*/
+    }
+});
